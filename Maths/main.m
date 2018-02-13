810df21ef5e87685bc664452fad970a280c95e84
Christoper <celoranta@gmail.com>
2018-02-13 14:20:24 -0800

    boilerplate

---
+++ Maths/main.m
@@ -0,0 +1,29 @@
+//
+//  main.m
+//  Maths
+//
+//  Created by Chris Eloranta on 2018-02-13.
+//  Copyright © 2018 Christopher Eloranta. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        
+        // create a command line game called Maths that will generate a random addition question
+        
+        // prompt the user to input their answer
+        
+        // parse the user's inputted answer and convert it to a primitive NSInteger
+        
+        // log "Right!" for correct and "Wrong!" for incorrect answers
+        
+        // present the next question immediately after the app outputs the evaluation of the user's input (for now there's no exit option)
+        
+        //  add the ability to exit the game
+        
+        // add a scoring function to the game
+    }
+    return 0;
+}
