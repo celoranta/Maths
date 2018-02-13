ee072947ff9b3653f1bfce56de0eafa028106ef4
Christoper <celoranta@gmail.com>
2018-02-13 15:24:00 -0800

    add exit function

--- Maths/main.m
+++ Maths/main.m
@@ -10,6 +10,7 @@
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
+
         while(true){
         // create a command line game called Maths that will generate a random addition question
         // prompt the user to input their answer
@@ -20,7 +21,8 @@ int main(int argc, const char * argv[]) {
             char userInput[255];
             NSLog(@"Please enter your answer");
             fgets(userInput,255,stdin);
-            NSString * userInputConverted = [NSString stringWithCString:userInput encoding:NSUTF8StringEncoding];
+            NSString *userInputConverted = [NSString stringWithCString:userInput encoding:NSUTF8StringEncoding];
+            if([userInputConverted isEqualToString:@"q\n"]) break;
             NSInteger userInputConvertedValue = [userInputConverted integerValue];
             NSInteger answer = mathValueOne + mathValueTwo;
             if (userInputConvertedValue == answer){
@@ -30,14 +32,7 @@ int main(int argc, const char * argv[]) {
                 NSLog(@"Ummm...  ok.  Try again, please.");
             }
             
-        
-        
-        // parse the user's inputted answer and convert it to a primitive NSInteger
-        
-        // log "Right!" for correct and "Wrong!" for incorrect answers
-        
-        // present the next question immediately after the app outputs the evaluation of the user's input (for now there's no exit option)
-        
+            
         //  add the ability to exit the game
         
 // add a scoring function to the game
