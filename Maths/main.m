8af249e67b765f995e4a5b3774a488a21eddc008
Christoper <celoranta@gmail.com>
2018-02-13 17:47:54 -0800

    Add function without classes

--- Maths/main.m
+++ Maths/main.m
@@ -16,14 +16,19 @@ int main(int argc, const char * argv[]) {
         // prompt the user to input their answer
             int mathValueOne = arc4random_uniform(100);
             int mathValueTwo = arc4random_uniform(100);
+
             NSLog(@"What is %i + %i?",mathValueOne,mathValueTwo);
             
             char userInput[255];
             NSLog(@"Please enter your answer");
             fgets(userInput,255,stdin);
+            NSCharacterSet *inputCharacterSet = [NSCharacterSet whitespaceAndNewlineCharacterSet];
             NSString *userInputConverted = [NSString stringWithCString:userInput encoding:NSUTF8StringEncoding];
-            if([userInputConverted isEqualToString:@"q\n"]) break;
-            NSInteger userInputConvertedValue = [userInputConverted integerValue];
+            NSString *userInputTrimmed = [userInputConverted stringByTrimmingCharactersInSet:inputCharacterSet];
+            
+
+            if([userInputTrimmed isEqualToString:@"q"]) break;
+            NSInteger userInputConvertedValue = [userInputTrimmed integerValue];
             NSInteger answer = mathValueOne + mathValueTwo;
             if (userInputConvertedValue == answer){
                 NSLog(@"You sexy genius!!");
